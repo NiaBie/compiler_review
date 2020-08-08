d38abb8007c6d4d48f633b15fb162ba1026d6a52
lynx <2104934038@qq.com>
2020-08-08 23:36:59 +0800

    bug: 拖动进度条

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 var host = "localhost:6061"
+var mouse_down = 0;
+
+function my_load() {
+  document.body.addEventListener("mousedown", function() {
+    mouse_down = 1;
+    console.log(mouse_down);
+  });
+  document.body.addEventListener("mouseup", function() {
+    mouse_down = 0;
+    console.log(mouse_down);
+  });
+  document.body.addEventListener("mouseout", function() {
+    mouse_down = 0;
+    console.log(mouse_down);
+  });
+}
 
 function load_video(video_name, hour, minute, second) {
   var main_video = document.getElementById("main_video");
@@ -6,11 +22,9 @@ function load_video(video_name, hour, minute, second) {
   var currentTime =  hour * 3600 + minute * 60 + second;
   main_video.addEventListener("loadedmetadata", function() {
     main_video.currentTime = currentTime;
-    console.log(currentTime);
-    console.log(main_video.currentTime);
     main_video.addEventListener("loadedmetadata", function() {
       // do nothing
-    })
+    });
   });
   main_video.load();
   main_video.play();
@@ -38,4 +52,34 @@ function pause_video() {
     main_video.play();
     pause_button.innerText = "pause";
   }
+}
+
+function change_progress() {
+  if (!mouse_down) return;
+  var event = window.event;
+  var main_video = document.getElementById("main_video");
+  
+  var mouse_x = event.clientX + document.body.scrollLeft;
+  var total_x = parseFloat($("#total_progress").css("width"));
+  var start_x = $("#total_progress").offset().left;
+  var percent = (mouse_x - start_x) / total_x;
+  if (isNaN(main_video.duration) == false) {
+    // console.log(mouse_x, total_x, start_x, percent);
+    main_video.currentTime = main_video.duration * percent;
+    $("#cur_progress").css("width", (total_x * percent) + "px");
+  }
+}
+
+function change_speed() {
+  if (!mouse_down) return;
+  var main_video = document.getElementById("main_video");
+  var event = window.event;
+  
+  var mouse_x = event.clientX + document.body.scrollLeft;
+  var total_x = parseFloat($("#total_speed").css("width"));
+  var start_x = $("#total_speed").offset().left;
+  var percent = (mouse_x - start_x) / total_x;
+  main_video.playbackRate = 15 * percent + 1;
+  console.log(main_video.playbackRate);
+  $("#cur_speed").css("width", (total_x * percent) + "px");
 }
