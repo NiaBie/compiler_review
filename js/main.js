381f2beb0adad50171d743ea7c7d3fdb5bc9e76a
lynx <2104934038@qq.com>
2020-08-08 21:52:51 +0800

    todo: 进度条

--- js/main.js
+++ js/main.js
@@ -1,18 +1,41 @@
 var host = "localhost:6061"
 
-function load_video(video_name, time = 0) {
-  var main_vide = document.getElementById("main_video");
-  main_vide.src = host + "/" + video_name + ".mp4";
+function load_video(video_name, hour, minute, second) {
+  var main_video = document.getElementById("main_video");
+  main_video.src = "/source/" + video_name + ".mp4";
+  var currentTime =  hour * 3600 + minute * 60 + second;
+  main_video.addEventListener("loadedmetadata", function() {
+    main_video.currentTime = currentTime;
+    console.log(currentTime);
+    console.log(main_video.currentTime);
+    main_video.addEventListener("loadedmetadata", function() {
+      // do nothing
+    })
+  });
+  main_video.load();
+  main_video.play();
 }
 
 function switch_video() {
-  var video_button = document.getElementById("switch_video");
+  var switch_button = document.getElementById("switch_video");
   var main_video = document.getElementById("main_video");
-  if (video_button.innerText == "show") {
+  if (switch_button.innerText == "show") {
     $(main_video).css("display", "block");
-    video_button.innerText = "hide";
+    switch_button.innerText = "hide";
   } else {
     $(main_video).css("display", "none");
-    video_button.innerText = "show";
+    switch_button.innerText = "show";
+  }
+}
+
+function pause_video() {
+  var pause_button = document.getElementById("pause_video");
+  var main_video = document.getElementById("main_video");
+  if (pause_button.innerText == "pause") {
+    main_video.pause();
+    pause_button.innerText = "play";
+  } else {
+    main_video.play();
+    pause_button.innerText = "pause";
   }
 }
